1ba753a9324e9bd58894e6480823fd08a3ce3604
Matias <matias.aleksandrowicz@gmail.com>
2025-02-26 14:31:41 -0300

    Solved 217. Contains Duplicate

---
+++ src/Easy/ContainsDuplicates217.java
@@ -0,0 +1,14 @@
+package Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicates217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        return numSet.size() < nums.length;
+    }
+}
