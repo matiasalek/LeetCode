874eb2aee795cecdfdcf97d36bc3a69a9b73bd30
Matias <matias.aleksandrowicz@gmail.com>
2025-03-03 01:26:18 -0300

    Improved solution at 3110

--- src/Easy/ScoreOfAString3110.java
+++ src/Easy/ScoreOfAString3110.java
@@ -4,16 +4,14 @@ import java.lang.Math;
 
 public class ScoreOfAString3110 {
     public int scoreOfString(String s) {
-        char c;
-        int result = 0;
-        int result2;
+        int score = 0;
+        int partial;
         for (int i = 0; i < s.length(); i++) {
             if (i+1 < s.length()) {
-                c = s.charAt(i);
-                result2 = Math.abs(c - s.charAt(i + 1));
-                result = result + result2;
+                partial = Math.abs(s.charAt(i) - s.charAt(i + 1));
+                score = score + partial;
             }
         }
-        return result;
+        return score;
     }
 }
