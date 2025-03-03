dc3970dc32b8f0ac3e55469a83482923f309799a
Matias <matias.aleksandrowicz@gmail.com>
2025-03-03 01:47:50 -0300

    Solved 2469. ConvertTheTemperature

---
+++ src/Easy/ConverttheTemperature2469.java
@@ -0,0 +1,9 @@
+package Easy;
+
+public class ConverttheTemperature2469 {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[]{kelvin, fahrenheit};
+    }
+}
