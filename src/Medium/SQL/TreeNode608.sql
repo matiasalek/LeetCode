f9261dc1b2b42c13cdee89fdaea5a1fb391bb0a3
Matias Aleksandrowicz <31048494+matiasalek@users.noreply.github.com>
2025-09-29 23:37:54 -0300

    Create TreeNode608.sql

---
+++ src/Medium/SQL/TreeNode608.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT id,
+CASE
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id IS NOT NULL AND id IN (SELECT DISTINCT p_id FROM Tree) THEN 'Inner'
+    ELSE 'Leaf' END AS type
+FROM Tree
